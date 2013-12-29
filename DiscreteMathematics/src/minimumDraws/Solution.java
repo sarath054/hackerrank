bc631cd53021587746d9a8d77efe82b509e57b1e
Sarath Chandra <sarath@gwynniebee.com>
2013-12-29 09:23:35 +0530

    minimum-draws solution

---
+++ DiscreteMathematics/src/minimumDraws/Solution.java
@@ -0,0 +1,20 @@
+package minimumDraws;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sarath
+ * Date: 12/29/13
+ * Time: 9:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(0<T--){
+            System.out.println(sc.nextInt()+1);
+        }
+    }
+}
